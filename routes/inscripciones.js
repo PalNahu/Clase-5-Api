97f877451325ac252c25628b354d8f124310e170
PalNahu <nahuelmartin.palma@estudiantes.unahur.edu.ar>
2023-10-08 17:57:45 -0400

    Agregada la paginacion

--- routes/inscripciones.js
+++ routes/inscripciones.js
@@ -1,104 +1,106 @@
-const models = require('../models');
+var express = require("express");
+var router = express.Router();
+var models = require("../models");
 
-const getInscripciones = (req, res, next) => {
-  const limit = parseInt(req.query.limit) || 10; // Cantidad de elementos por página (predeterminado: 10)
-  const page = parseInt(req.query.page) || 1; // Página actual (predeterminado: 1)
+router.get("/", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10; // Cantidad de elementos por página (predeterminado: 10)
+    const page = parseInt(req.query.page) || 1; // Página actual (predeterminado: 1)
+    const offset = (page - 1) * limit;
 
-  const offset = (page - 1) * limit;
-
-  models.inscripcion
-    .findAndCountAll({
-      attributes: ["id", "id_alumno", "id_materia"],
+    const { count, rows: inscripciones } = await models.inscripciones.findAndCountAll({
+      attributes: ["id"],
       include: [
-        { as: 'Inscripcion-Alumno-Relacion', model: models.alumno, attributes: ["id", "nombre", "apellido"] },
-        { as: 'Inscripcion-Materia-Relacion', model: models.materia, attributes: ["id", "nombre"] }
+        {
+          model: models.materia,
+          as: "inscripcion-materia",
+          attributes: ["id", "nombre"],
+        },
+        {
+          model: models.alumnos,
+          as: "inscripcion-alumno",
+          attributes: ["id", "nombre", "apellido"],
+        },
       ],
       offset: offset,
       limit: limit,
-      subQuery: false,
-    })
-    .then((result) => {
-      const inscripciones = result.rows;
-      const totalRecords = result.count;
-      const totalPages = Math.ceil(totalRecords / limit);
-
-      res.send({
-        inscripciones,
-        pagination: {
-          page,
-          limit,
-          totalRecords,
-          totalPages,
-        },
-      });
-    })
-    .catch((error) => {
-      return next(error);
     });
-};
 
-const createInscripcion = (req, res) => {
-  models.inscripcion
-    .create({ id_alumno: req.body.id_alumno, id_materia: req.body.id_materia })
-    .then((inscripcion) => res.status(201).send({ id: inscripcion.id }))
-    .catch((error) => {
-      if (error === 'SequelizeUniqueConstraintError: Validation error') {
-        res
-          .status(400)
-          .send('Bad request: existe otra inscripcion con el mismo nombre');
-      } else {
-        console.log(`Error al intentar insertar en la base de datos: ${error}`);
-        res.sendStatus(500);
+    const totalPages = Math.ceil(count / limit);
+
+    res.send({
+      inscripciones,
+      pagination: {
+        page,
+        limit,
+        totalRecords: count,
+        totalPages,
+      },
+    });
+  } catch (error) {
+    console.error(`Error al obtener las inscripciones: ${error}`);
+    res.sendStatus(500);
+  }
+});
+
+
+router.post("/", (req, res) => {
+  console.log(req);
+  models.inscripciones
+    .create({ id_materia: req.body.id_materia, id_alumno: req.body.id_alumno })
+    .then(inscripcion => res.status(201).send({ id: inscripcion.id }))
+    .catch(error => {
+      if (error.name === "SequelizeUniqueConstraintError") {
+        res.status(400).send('Bad request: Ya existe una inscripción con el mismo id_materia e id_alumno')
+      }
+      else {
+        console.log(`Error al intentar insertar en la base de datos: ${error}`)
+        res.sendStatus(500)
       }
     });
-};
+});
 
 const findInscripcion = (id, { onSuccess, onNotFound, onError }) => {
-  models.inscripcion
+  models.inscripciones
     .findOne({
-      attributes: ['id', 'id_alumno', 'id_materia'],
-      where: { id },
+      attributes: ["id", "id_materia", "id_alumno"],
+      where: { id }
     })
-    .then((inscripcion) => (inscripcion ? onSuccess(inscripcion) : onNotFound()))
+    .then(inscripcion => (inscripcion ? onSuccess(inscripcion) : onNotFound()))
     .catch(() => onError());
 };
 
-const getInscripcion = (req, res) => {
+router.get("/:id", (req, res) => {
   findInscripcion(req.params.id, {
-    onSuccess: (inscripcion) => res.send(inscripcion),
+    onSuccess: inscripcion => res.send(inscripcion),
     onNotFound: () => res.sendStatus(404),
-    onError: () => res.sendStatus(500),
+    onError: () => res.sendStatus(500)
   });
-};
+});
 
-const putInscripcion = (req, res) => {
-  const { id_alumno, id_materia } = req.body;
-  const update = {};
-  if (id_alumno) update.id_alumno = id_alumno;
-  if (id_materia) update.id_materia = id_materia;
-  const onSuccess = (inscripcion) =>
+router.put("/:id", (req, res) => {
+  const onSuccess = inscripcion =>
     inscripcion
-      .update(update)
+      .update({ id_materia: req.body.id_materia, id_alumno: req.body.id_alumno }, { fields: ["id_materia", "id_alumno"] })
       .then(() => res.sendStatus(200))
-      .catch((error) => {
-        if (error === 'SequelizeUniqueConstraintError: Validation error') {
-          res
-            .status(400)
-            .send('Bad request: existe otra inscipcion con el mismo Alumno y Materia');
-        } else {
-          console.log(`Error al intentar actualizar la base de datos: ${error}`);
-          res.sendStatus(500);
+      .catch(error => {
+        if (error.name === "SequelizeUniqueConstraintError") {
+          res.status(400).send('Bad request: Ya existe una inscripción con el mismo id_materia e id_alumno')
+        }
+        else {
+          console.log(`Error al intentar actualizar la base de datos: ${error}`)
+          res.sendStatus(500)
         }
       });
   findInscripcion(req.params.id, {
     onSuccess,
     onNotFound: () => res.sendStatus(404),
-    onError: () => res.sendStatus(500),
+    onError: () => res.sendStatus(500)
   });
-};
+});
 
-const deleteInscripcion = (req, res) => {
-  const onSuccess = (inscripcion) =>
+router.delete("/:id", (req, res) => {
+  const onSuccess = inscripcion =>
     inscripcion
       .destroy()
       .then(() => res.sendStatus(200))
@@ -106,14 +108,8 @@ const deleteInscripcion = (req, res) => {
   findInscripcion(req.params.id, {
     onSuccess,
     onNotFound: () => res.sendStatus(404),
-    onError: () => res.sendStatus(500),
+    onError: () => res.sendStatus(500)
   });
-};
+});
 
-module.exports = {
-  getInscripcion,
-  getInscripciones,
-  createInscripcion,
-  putInscripcion,
-  deleteInscripcion,
-};
+module.exports = router;
