52778ce72a4159f1e847ba75f24e5e12f45077a1
PalNahu <nahuelmartin.palma@estudiantes.unahur.edu.ar>
2023-11-14 11:16:18 -0500

    Agregado de Login con registro de auditoria con Id e IP

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var inscripcionesRouter = require('./routes/inscripciones');
 var app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./settings/swagger-config"); 
-
+const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2/promise');
+var requestIp = require('request-ip');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Login setup
+app.use(requestIp.mw());
+
+const dbConfig ={
+  host: 'localhost',
+  user: 'usuariosuper',
+  password: '12344',
+  database: 'api_v1',
+};
+
+app.use(async(req, res, next) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const ip = req.clientIp; 
+
+      await connection.execute('INSERT INTO auditoria (ip) VALUES (?)' , [ip]);
+
+    await connection.end();
+    next();
+  } catch (error) {
+    console.error('Error al loguear la petición', error);
+    next(error);
+  }
+});
+
+
+//Login setup
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/car', carrerasRouter);
